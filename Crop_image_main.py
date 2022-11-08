b0b54a19e903174dac4868c5013cf72167aa612f
thibaulttabarin <thibault.tabarin@gmail.com>
2022-11-07 18:27:39 -0700

    include boundary truncature issue #4

--- Crop_image_main.py
+++ Crop_image_main.py
@@ -15,6 +15,7 @@ import json
 import numpy as np
 import argparse
 from PIL import Image, ImageFile
+import matplotlib.pyplot as plt
 
 # Some of the image are truncated and trigger an error the follow line solve the problem
 ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -46,6 +47,48 @@ def get_bbox(metadata_file):
              
     return bbox
 
+def Crop(image, bbox, increase=0.05):
+    '''
+    Crop the image using the bounding box, expected format [left,top,right,bottom] and adding
+    a increase in size vertically and horizontally
+    the function will cutoff the crop to match the boundary of the image if the increase go over the 
+    image boundary
+
+    Parameters
+    ----------
+    image : PIL image format
+        DESCRIPTION. image imported using PIL.Image.open(image_file)
+    bbox : list
+        DESCRIPTION. list with bbox coordinate format [left,top,right,bottom]
+    increase : float, optional
+        DESCRIPTION. The default is 0.05. increase of the size of the box around the fish per dimension
+        width + 5% and height + 5%
+
+    Returns
+    -------
+    im1 : TYPE
+        DESCRIPTION.
+
+    '''
+    
+    # 5% increase by default of the bbox, metadata bbox is very tight sometime too tight
+    # increase factor in each direction (width height)
+    factor = increase/2
+    left,top,right,bottom = bbox
+   
+    h_increase = int(abs((right-left)*factor))
+    v_increase = int(abs((bottom-top)*factor))
+    new_bbox = (left-h_increase, top-v_increase, right+h_increase, bottom+v_increase)
+    
+    # cutoff the cropping to the original image boundary
+    image_limit = (0,0)+image.size
+    new_bbox_cutoff_1 = map(np.maximum, new_bbox[0:2], (0,0))
+    new_bbox_cutoff_2 = map(np.minimum, new_bbox[2:4], image.size)
+    new_bbox = tuple(new_bbox_cutoff_1) + tuple(new_bbox_cutoff_2)
+    
+    im1 = image.crop(new_bbox) # bbox (left,top,right,bottom)
+    return im1
+
 def main(image_file, metadata_file, output_file, increase=0.05):
     '''
     Extract the fish bbox from the metadatafile.json and crop the fish and save the result in outputfile
@@ -58,7 +101,7 @@ def main(image_file, metadata_file, output_file, increase=0.05):
         DESCRIPTION. filename location of the metadata file .json
     output_file : string
         DESCRIPTION. filename location where to save the cropped image
-    increase : int, optional
+    increase : float, optional
         DESCRIPTION. The default is 0.05. increase of the size of the box around the fish per dimension
         width + 5% and height + 5%
 
@@ -74,14 +117,8 @@ def main(image_file, metadata_file, output_file, increase=0.05):
     bbox = get_bbox(metadata_file)
 
     if bbox:
-        # 5% increase by default of the bbox, metadata bbox is very tight sometime too tight
-        # increase factor in each direction (width height)
-        factor = increase/2
-        left,top,right,bottom = bbox
-        h_increase = int(abs((right-left)*factor))
-        v_increase = int(abs((bottom-top)*factor))
-        new_bbox = (left-h_increase, top-v_increase, right+h_increase, bottom+v_increase)
-        im1 = im.crop(new_bbox) # bbox (left,top,right,bottom)
+        # use the crop function
+        im1 = Crop(im, bbox, increase=increase)
 
     else:
         # if no bounding box detected
@@ -91,21 +128,18 @@ def main(image_file, metadata_file, output_file, increase=0.05):
     im1.save(output_file)
 
 def argument_parser():
-    parser = argparse.ArgumentParser(description='Crop the fish image from metadata.json.')
-    parser.add_argument('input_image', help='Path of input original fish image format JPG image file.')
-    parser.add_argument('input_metadata', help='Path of input drexel_metadata_reformatted format JSON metadata file.')
-    parser.add_argument('output', help='Path of output cropped fish image format JPG image file.')
+    parser = argparse.ArgumentParser(description='Crop the fish image using bounding bbox from a metadata.json.')
+    parser.add_argument('input_image', help='Path of input original fish image. Format JPG image file.')
+    parser.add_argument('input_metadata', help='Path of input drexel_metadata_reformatted. Format JSON metadata file.')
+    parser.add_argument('output', help='Path of output cropped fish image. Format JPG image file.')
+    parser.add_argument('--increase', type=float, default=0.05, 
+                        help='size increase apply to the bounding box for cropping (in width and height).Default 0.05. Format float.')
     return parser
     
-def show_usage():
-    
-    print()
-    print(f'Usage : {sys.argv[0]} <original_image.jpg> <metadata.json> <cropped_image.jpg>\n')
-    print()
 
 if __name__ == '__main__':
     
     parser = argument_parser()
     args = parser.parse_args()
-    main(args.input_image, args.input_metadata, args.output)
+    main(args.input_image, args.input_metadata, args.output, increase=args.increase)
         
